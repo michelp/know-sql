bbe58f0e868529474f61fac4d2408e88c04ad028
Michel Pelletier <pelletier.michel@gmail.com>
2016-04-14 14:57:49 -0700

    arg parsing

--- ksql/install.sql
+++ ksql/install.sql
@@ -2,6 +2,7 @@ BEGIN;
 DROP SCHEMA IF EXISTS http CASCADE;
 CREATE SCHEMA http;
 
+
 CREATE FUNCTION http.get(
     aschema text,
     afunction text,
@@ -9,17 +10,17 @@ CREATE FUNCTION http.get(
     apath jsonb,
     args jsonb DEFAULT '{}') RETURNS json AS $$
 DECLARE
-  body text;
-  result json;
+    body text;
+    result json;
 BEGIN
     body := format('SELECT array_to_json(array_agg(row_to_json(row, true)), true) '
                    'FROM %I.%I($1, $2, $3) as row;',
                    aschema, 'get_' || afunction);
-    RAISE NOTICE '%', body;
     EXECUTE body INTO result USING environ, apath, args;
     RETURN result;
 END;
 $$ LANGUAGE plpgsql;
+
 
 CREATE FUNCTION http.post(
     aschema text,
@@ -29,35 +30,37 @@ CREATE FUNCTION http.post(
     args jsonb DEFAULT '{}',
     request_body bytea DEFAULT '') RETURNS json AS $$
 DECLARE
-  body text;
-  result json;
+    body text;
+    result json;
 BEGIN
     body := format('SELECT array_to_json(array_agg(row_to_json(row, true)), true)
                     FROM %I.%I($1, $2, $3, $4) as row;',
                    aschema, 'get_' || afunction);
-    RAISE NOTICE '%', body;
     EXECUTE body INTO result USING environ, apath, args, request_body;
     RETURN result;
 END;
 $$ LANGUAGE plpgsql;
 
 
-CREATE FUNCTION http.get_test(jsonb, jsonb, jsonb) RETURNS table(environ jsonb, path jsonb, args jsonb) AS $$
-       select $1, $2, $3;
-$$ LANGUAGE SQL;
-
-CREATE FUNCTION http.post_test(jsonb, jsonb, jsonb, bytea) RETURNS table(environ jsonb, path jsonb, args jsonb, request bytea) AS $$
-       select $1, $2, $3, $4;
+CREATE FUNCTION http.get_test(jsonb, jsonb, jsonb) 
+    RETURNS table(environ jsonb, path jsonb, args jsonb) AS $$
+    select $1, $2, $3;
 $$ LANGUAGE SQL;
 
 
-CREATE FUNCTION http.get_test_not_found(jsonb, jsonb, jsonb) RETURNS table(environ jsonb, path jsonb, args jsonb) AS $$
+CREATE FUNCTION http.post_test(jsonb, jsonb, jsonb, bytea) 
+    RETURNS table(environ jsonb, path jsonb, args jsonb, request bytea) AS $$
+    select $1, $2, $3, $4;
+$$ LANGUAGE SQL;
+
+
+CREATE FUNCTION http.get_test_not_found(jsonb, jsonb, jsonb) 
+    RETURNS table(environ jsonb, path jsonb, args jsonb) AS $$
 DECLARE
 BEGIN
-  RAISE '404';
-  RETURN QUERY SELECT $1, $2, $3;
+    RAISE '404';
+    RETURN QUERY SELECT $1, $2, $3;
 end;
-
 $$ LANGUAGE plpgsql;
 
 COMMIT;
--- ksql/server.py
+++ ksql/server.py
@@ -1,3 +1,16 @@
+"""Know-SQL
+
+A simple json API proxy over HTTP to Postgres.
+
+Usage:
+  ksql.py
+
+Options:
+  -h --help     Show this screen.
+  -d --database Database connection string.
+
+"""
+
 import os
 import re
 from json import dumps
@@ -16,42 +29,50 @@ location_re = re.compile(
     r"/(?P<db>\w+)/(?P<schema>\w+)/(?P<function>\w+)(?P<path>/.*){0,}$"
     )
 
-@Request.application 
-def app(request): 
-    conn.rollback() 
-    try:
-        groups = location_re.match(request.path).groups()
-        if not groups:
-            raise Exception('badz')
-        db, schema, func_name, path = groups
-        path = '' if path is None else path
-
-        environ = Json({k: v for k, v in request.environ.items() if k.isupper()})
-        path = Json(filter(None, path.split('/')))
-        args = Json(dict(request.args))
-
-        method = request.method
-        proc_args = (schema, func_name)
-        if method == 'GET':
-            proc_args = proc_args + (environ, path, args)
-        elif method == 'POST':
-            proc_args = proc_args + (environ, path, args, request.get_data())
-        with conn.cursor() as cur:
-            cur.callproc('http.get', proc_args)
-            conn.commit()
-            result = cur.fetchone()[0]
-            response = Response(dumps(result), mimetype='application/json')
-            response.cache_control.max_age = 300
-            return response
-    except psycopg2.Error, e:
+def make_app(dsn):
+    @Request.application 
+    def app(request): 
+        conn.rollback() 
         try:
-            conn.rollback()
-            logger.exception(request.path)
-            code = int(e.diag.message_primary)
-        except Exception:
-            raise InternalServerError
-        raise default_exceptions.get(code, InternalServerError)
+            groups = location_re.match(request.path).groups()
+            if not groups:
+                raise Exception('badz')
+            db, schema, func_name, path = groups
+            path = '' if path is None else path
+
+            environ = Json({k: v for k, v in request.environ.items() if k.isupper()})
+            path = Json(filter(None, path.split('/')))
+            args = Json(dict(request.args))
+
+            method = request.method
+            proc_args = (schema, func_name)
+            if method == 'GET':
+                proc_args = proc_args + (environ, path, args)
+            elif method == 'POST':
+                proc_args = proc_args + (environ, path, args, request.get_data())
+            with conn.cursor() as cur:
+                cur.callproc('http.get', proc_args)
+                conn.commit()
+                result = cur.fetchone()[0]
+                response = Response(dumps(result), mimetype='application/json')
+                response.cache_control.max_age = 300
+                return response
+        except psycopg2.Error, e:
+            try:
+                conn.rollback()
+                logger.exception(request.path)
+                code = int(e.diag.message_primary)
+            except Exception:
+                raise InternalServerError
+            raise default_exceptions.get(code, InternalServerError)
+    return app
         
 if __name__ == '__main__':
+    from docopt import docopt
     from werkzeug.serving import run_simple
-    run_simple('0.0.0.0', 4000, app, use_debugger=True)
+    arguments = docopt(__doc__, version='Know-SQL 0.1')
+    import pdb; pdb.set_trace
+    bind = arguments.get('--bind', '0.0.0.0')
+    port = arguments.get('--port', 4000)
+    dsn = arguments.get('--database', '')
+    run_simple(bind, port, make_app(dsn), use_debugger=True)
